016e62f2b64152f26eaa8e6d0beb5e53a57deb13
Oussama Hichou <56653051+usmhic@users.noreply.github.com>
2022-08-09 20:23:58 +0100

    Update caprover_installer.sh

--- caprover_installer.sh
+++ caprover_installer.sh
@@ -13,6 +13,6 @@ sudo chmod +x docker.sh docker_compose.sh
 ./docker_compose.sh
 sudo docker run -p 80:80 -p 443:443 -p 3000:3000 -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
 sudo npm install -g caprover
-sudo apt update
+sudo apt update -y
 sleep 60s
 sudo caprover serversetup
